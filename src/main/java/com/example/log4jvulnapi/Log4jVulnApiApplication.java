ddce965ae63ff6ca59a9effa304f5fc64bb6739b
test03052001 <test03052001@gmail.com>
2026-04-30 18:12:02 +0530

    files added

---
+++ src/main/java/com/example/log4jvulnapi/Log4jVulnApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.log4jvulnapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4jVulnApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Log4jVulnApiApplication.class, args);
+    }
+}
+
---
+++ src/main/java/com/example/log4jvulnapi/controller/Log4jDemoController.java
@@ -0,0 +1,21 @@
+package com.example.log4jvulnapi.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Log4jDemoController {
+    private static final Logger log = LogManager.getLogger(Log4jDemoController.class);
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(defaultValue = "world") String name) {
+        log.info("Hello endpoint called with name={}", name);
+        return "Hello, " + name;
+    }
+}
+
